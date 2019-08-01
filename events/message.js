080732f5a696a067d58ed3158f6a9323b03f7c30
Tuned Chaos LLC <lane@tunedchaos.com>
2019-08-01 19:11:49 -0400

    Updated the commands to be case insensitive

--- events/message.js
+++ events/message.js
@@ -8,31 +8,32 @@ const position = require('../commands/position')
 module.exports = (client, message) => {
     if(message.channel.id === process.env.CHANNEL_ID && message.author.id !== process.env.BOT_USER_ID)
     {
-        if (message.content.includes("!add"))
+        var messageContent = message.content.toLowerCase()
+        if (messageContent.includes("!add"))
         {
             return add(message)
         }
-        else if(message.content.includes("!status"))
+        else if(messageContent.includes("!status"))
         {
             return status(message)
         }
-        else if(message.content.includes("!playing"))
+        else if(messageContent.includes("!playing"))
         {
             return playing(message)
         }
-        else if(message.content.includes("!list"))
+        else if(messageContent.includes("!list"))
         {
             return list(message)
         }
-        else if(message.content.includes("!next"))
+        else if(messageContent.includes("!next"))
         {
             return next(message)
         }
-        else if(message.content.includes("!queue"))
+        else if(messageContent.includes("!queue"))
         {
             return queue(message)
         }
-        else if(message.content.includes("!position"))
+        else if(messageContent.includes("!position"))
         {
             return position(message)
         }
